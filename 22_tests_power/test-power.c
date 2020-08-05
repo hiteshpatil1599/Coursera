d0b9faa90671b416b4109d77925fb7e29853a4d2
Coursera Learner <nobody@nowhere.nul>
2020-08-05 15:04:24 +0000

    done

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+unsigned power(unsigned, unsigned);
+
+int main()
+{
+  if(power(0,0)!=1){/*printf("power1\n")*/;return EXIT_FAILURE;}//3
+  if(power(1,0)!=1){/*printf("power2\n")*/;return EXIT_FAILURE;}//2
+  if(power(1,1)!=1){/*printf("power3\n")*/;return EXIT_FAILURE;}//6,7,9
+  if(power(2,1)!=2){/*printf("power4\n")*/;return EXIT_FAILURE;}//1
+  if(power(5,2)!=25){/*printf("power5\n")*/;return EXIT_FAILURE;}//8,5,10
+  if(power(-1,1)!=-1){/*printf("power6\n")*/;return EXIT_FAILURE;}//
+  return EXIT_SUCCESS;
+}
