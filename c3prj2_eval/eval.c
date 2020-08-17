a1d4fefafe5f6f66757536ef23a933423154d63e
Coursera Learner <nobody@nowhere.nul>
2020-08-17 08:01:11 +0000

    done

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -213,7 +213,7 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 	  return c1.cards[0][0].value - c2.cards[0][0].value;
 	  
 	case THREE_OF_A_KIND:
-	  return c1.cards[0][0].value - c2.cards[0][0].value;
+	  
 	  
 	case TWO_PAIR:
 	  for(int i=0,flag=0; i<5; i++)
